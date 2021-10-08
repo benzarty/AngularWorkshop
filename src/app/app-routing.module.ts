3ceb2bc78045b410031bee422073d795b4da270e
benzarty <m.benzarti.1996@gmail.com>
2021-10-08 23:15:18 +0100

    First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { ListUserComponent } from './list-user-component/list-user-component.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { MainProviderComponent } from './main-provider/main-provider.component';
+import { MainProductComponent } from './main-product/main-product.component';
+import { MainUserComponent } from './main-user/main-user.component';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'mainuser',
+    component: MainUserComponent,
+    children: [{ path: 'listuser/:cat', component: ListUserComponent }],
+    // [{ path: 'listuser/:cat/:test', component: ListUserComponent }],
+  },
+  { path: 'mainproduct', component: MainProductComponent },
+  { path: 'mainprovider', component: MainProviderComponent },
+
+  { path: '**', component: NotFoundPageComponent },
+];
+// yemchi 9ad 9ad lien
+//prefix to learn
+// lezem ** le5era 5ater yemchi bil ordre w gadina '' 5ater bech yechargi notfound ken gedouch redirect
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TestComponent } from './Test/Test.component';
+import { ListUserComponent } from './list-user-component/list-user-component.component';
+import { FormsModule } from '@angular/forms';
+import { MainUserComponent } from './main-user/main-user.component';
+import { MainProductComponent } from './main-product/main-product.component';
+import { MainProviderComponent } from './main-provider/main-provider.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { HomeComponent } from './home/home.component';
+
+//un composant fait partie d'une module
+@NgModule({     //bech te9oul héthi module  (declariw fih les module eli fih)
+  declarations: [			
+    AppComponent,   //(imports chnowa module héthéké 7achtou bih(modules))
+      TestComponent,
+      ListUserComponent,
+      MainUserComponent,
+      MainProductComponent,
+      MainProviderComponent,
+      HeaderComponent,
+      FooterComponent,
+      NotFoundPageComponent,
+      HomeComponent, //leezm kol component lese9 fi module
+      
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule, //bech te5dem bi ngmodul
+    NgbModule
+  ],
+  providers: [],  //service
+  bootstrap: [AppComponent] //bech yebda mina 
+  //le composant racine qu'Angular crée et insère dans laindex.htmlpage Web hôte.
+})
+export class AppModule { }
+
+//module bech yejama3 les composants (hakéké arte7 lil applicarion bech ki chargi module yecharji eli yeste7a9ou kol fi module héthéké )
