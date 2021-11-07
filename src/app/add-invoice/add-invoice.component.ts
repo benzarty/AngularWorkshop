f9f5cda2724d853824cd19f677df8edfb518197d
Mohamed benzarti <m.benzarti.1996@gmail.com>
2021-11-07 14:20:41 +0100

    com1

--- src/app/add-invoice/add-invoice.component.ts
+++ src/app/add-invoice/add-invoice.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Invoice } from '../models/invoice';
 
 @Component({
   selector: 'app-add-invoice',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddInvoiceComponent implements OnInit {
 
+  myForm :FormGroup;
+  @Output() aded = new EventEmitter<Invoice>();
   constructor() { }
 
   ngOnInit(): void {
+    this.myForm=new FormGroup({
+      idInvoice:new FormControl(),
+      discountAmount: new FormControl(),
+      billAmount: new FormControl(),
+      dateBill: new FormControl(),
+      Status: new FormControl(),
+    }) 
   }
-
+add(){
+this.aded.emit(this.myForm.value);
+this.myForm.reset();
+}
 }
--- src/app/list-invoice/list-invoice.component.ts
+++ src/app/list-invoice/list-invoice.component.ts
@@ -7,23 +7,40 @@ import { Invoice } from '../models/invoice';
   styleUrls: ['./list-invoice.component.css']
 })
 export class ListInvoiceComponent implements OnInit {
-  invoices: Invoice[];
-
- 
-
+  invoices:Invoice[];
+  invoiceToEditParent:Invoice;
+  showAdd = false;
+  nb=5;
   constructor() { }
-
+  show=false;
   ngOnInit(): void {
     this.invoices =[
-      { idInvoice: 1, discountAmount: 20, billAmount: 500, dateBill:
-      "14/07/2021" , Status : true,like : 20},
-      { idInvoice: 2, discountAmount: 10, billAmount: 1000, dateBill:
-      "02/10/2020" , Status : false,like : 20},
-      { idInvoice: 3, discountAmount: 50, billAmount: 200, dateBill:
-      "15/12/2021" , Status : false,like : 20},
+      { idInvoice: 1, discountAmount: 20, billAmount: 500, dateBill: "14/07/2021" , Status : true,like:50},
+      { idInvoice: 2, discountAmount: 10, billAmount: 1000, dateBill: "02/10/2020" , Status : false,like:50},
+      { idInvoice: 3, discountAmount: 50, billAmount: 200, dateBill: "15/12/2021" , Status : false,like:50},
+  
       ]
+  
   }
-  deleteUser(x: number) {
-    this.invoices.splice(x, 1);
+  editInvoice(x:Invoice){
+    this.show=true;
+    this.invoiceToEditParent=x;
+    
+  }
+  editMyInvoice(i:any){
+    for (let k in this.invoices){
+      if (this.invoices[k].idInvoice == i.idInvoice){
+        this.invoices[k]=i;
+      }
+    }
+
+
+    
+  }
+  showForm(){
+    this.showAdd=true;
+  }
+  addInvoice(i:Invoice){
+this.invoices.push(i);
   }
 }
